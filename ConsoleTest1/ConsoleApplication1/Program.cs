e5bfec8645fc2704e4d16a6b7f550fa250b0e6a8
helifreak <nbaker01@nbaker.com>
2015-01-16 14:00:24 -0800

    simple webserver

--- ConsoleTest1/ConsoleApplication1/Program.cs
+++ ConsoleTest1/ConsoleApplication1/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using SimpleWebServer;
 
 namespace ConsoleApplication1
 {
@@ -10,7 +12,17 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("FOO 3\n");
+            WebServer ws = new WebServer(SendResponse, "http://*:10500/test/");
+            ws.Run();
+            Console.WriteLine("A simple webserver. Press a key to quit.");
+            Console.ReadKey();
+            ws.Stop();
+        }
+
+        public static string SendResponse(HttpListenerRequest request)
+        {
+            return string.Format("<HTML><BODY>My web page.<br>{0}</BODY></HTML>", DateTime.Now);
         }
     }
 }
+
