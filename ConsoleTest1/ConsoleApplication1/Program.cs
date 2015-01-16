07139b65f6ee492e03a28d4e63a5eab7a5796b07
helifreak <nbaker01@nbaker.com>
2015-01-16 07:21:50 -0800

    Two

--- ConsoleTest1/ConsoleApplication1/Program.cs
+++ ConsoleTest1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("FOO\n");
+            Console.WriteLine("FOO 2\n");
         }
     }
 }
